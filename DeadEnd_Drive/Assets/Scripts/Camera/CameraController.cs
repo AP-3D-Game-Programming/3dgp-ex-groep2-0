91fd99ba19ea21f74d5106e350b5251766fed3b3
Spruyt Sem <s163017@student.ap.be>
2025-11-27 18:03:22 +0100

    laatste fixes van vandaag

--- DeadEnd_Drive/Assets/Scripts/Camera/CameraController.cs
+++ DeadEnd_Drive/Assets/Scripts/Camera/CameraController.cs
@@ -4,11 +4,7 @@ public class CameraController : MonoBehaviour
 {
     public float sensitivity = 300f;  // muisgevoeligheid
     public Transform player;          // sleep hier de Player in
-    public float defaultFOV = 60f;    // standaard FOV
-    public float fovSpeed = 20f;      // snelheid van FOV-aanpassing met scroll
-    public float minFOV = 30f;        // minimale FOV
-    public float maxFOV = 120f;       // maximale FOV
-
+    public float defaultFOV = 120;    // standaard FOV
     private float xRotation = 0f;
     private Camera cam;               // referentie naar de camera
 
@@ -36,15 +32,5 @@ public class CameraController : MonoBehaviour
         xRotation = Mathf.Clamp(xRotation, -80f, 80f);
         transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
 
-        // --- FOV aanpassen met scrollwheel ---
-        if (cam != null)
-        {
-            float scroll = Input.GetAxis("Mouse ScrollWheel");
-            if (scroll != 0f)
-            {
-                cam.fieldOfView += scroll * fovSpeed;
-                cam.fieldOfView = Mathf.Clamp(cam.fieldOfView, minFOV, maxFOV);
-            }
-        }
     }
 }
