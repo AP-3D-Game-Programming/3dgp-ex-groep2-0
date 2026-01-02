65308513aa48337359d0f8fb3823f8db0563f7a3
Jelle Haast <jelle.haast@gmail.com>
2026-01-03 00:04:18 +0100

    added the respawn points and despawning of the priest when close to tower in Level1JH

--- DeadEnd_Drive/Assets/priest/Scripts/SafeTrigger.cs
+++ DeadEnd_Drive/Assets/priest/Scripts/SafeTrigger.cs
@@ -2,12 +2,14 @@ using UnityEngine;
 
 public class SafeTrigger : MonoBehaviour
 {
-    public PriestManager manager;
+    public MonsterManager manager;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
+            Debug.Log("entered collider");
+            manager.enableAutoRespawn = false;
             manager.DespawnPriest();
         }
     }
