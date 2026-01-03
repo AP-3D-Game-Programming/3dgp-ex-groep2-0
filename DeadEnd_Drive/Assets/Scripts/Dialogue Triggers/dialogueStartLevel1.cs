afcc9c035d265090543e363e986d626e0ab7eb29
Renzo Raes <s158084@ap.be>
2026-01-03 20:54:58 +0100

    tekst op zelfde plek

--- DeadEnd_Drive/Assets/Scripts/Dialogue Triggers/dialogueStartLevel1.cs
+++ DeadEnd_Drive/Assets/Scripts/Dialogue Triggers/dialogueStartLevel1.cs
@@ -4,33 +4,46 @@ using System.Collections;
 
 public class StartDialogueLevel1 : MonoBehaviour
 {
+[Header("Sleep hier het TextMeshPro object met je tekst in")]
     public TMP_Text dialogueText;
+
+    [Header("Instellingen")]
     public float displayTime = 3f;
     public float fadeSpeed = 1f;
 
-    // Verander 'void Start' naar deze publieke functie
-    // Zorg dat het object in de Inspector wel gewoon AAN staat
+    // We gebruiken geen Start() meer, want TeleportSecScene roept dit nu aan.
+    
+    // Deze functie wordt aangeroepen door jouw TeleportSecScene script
     public void ShowDialogue()
     {
         if (dialogueText != null)
         {
-            dialogueText.gameObject.SetActive(true); // Voor de zekerheid
-            dialogueText.text = "I have to find the car, I don't feel right staying here";
+            // Zet de tekst aan en reset de zichtbaarheid
+            dialogueText.gameObject.SetActive(true);
             dialogueText.alpha = 1f;
+
+            // Start de timer om te faden
             StartCoroutine(FadeOut());
+        }
+        else
+        {
+            Debug.LogWarning("StartDialogueLevel1: Geen TextMeshPro object gekoppeld!");
         }
     }
 
     IEnumerator FadeOut()
     {
+        // 1. Wacht
         yield return new WaitForSeconds(displayTime);
 
+        // 2. Fade uit
         while (dialogueText.alpha > 0)
         {
             dialogueText.alpha -= Time.deltaTime * fadeSpeed;
             yield return null;
         }
 
+        // 3. Zet uit
         dialogueText.gameObject.SetActive(false);
     }
 }
